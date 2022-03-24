5236987f9c829c805ee1e947dd0ae5443542617b
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-03-24 19:23:01 +0300

    Counter setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+  return (
+    <div className="container">
+      <p className="counter">...</p>
+
+      <div className="buttonsContainer">
+        <button className="button">
+          Increment
+        </button>
+
+        <button className="button">
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
